2e82d7a490573f6248869c13288e48941b4d1436
RoundQube <naushad.kasu@gmail.com>
2017-09-19 16:07:22 -0500

    Added magnitude script

---
+++ market_move_magnitude.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import csv
+import argparse
+from datetime import datetime
+from datetime import timedelta
+
+def calculateRangeBasedMagnitude(filename, days, multiplier):
+    '''
+    calculate range-based by points or percentages moved
+    '''
+    index = 0
+    breached = 0
+    currentPrice = 0
+    counter = 0
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        next(reader, None)  # skip header
+        rows = list(reader)
+        for row in rows[counter:]:
+            currentPrice = float(rows[counter][4])
+            counter = counter + 1
+            index = counter
+            shift = index + days
+            for row in rows[index:shift]:
+                index = index + 1
+                try:
+                    startDate = rows[counter][0]
+                    startDateClose = float(rows[counter][4])
+                    endDate = rows[index][0]
+                    endDateClose = float(rows[index][4])
+                    move = endDateClose - startDateClose
+
+                    if(move >= (currentPrice * multiplier)):
+                        print("Start Date: %s, Start Date Close: %.2f, End Date: %s, End Date Close: %.2f, Move: %.2f" % (startDate, startDateClose, endDate, endDateClose, move))
+                        breached = breached + 1
+                except IndexError:
+                    break
+
+        percentageBreached = float(breached) / counter * 100
+        print("Breached: %d out of %d (%.2f%s)" % (breached, counter, percentageBreached, "%"))
+            
+def calculateMagnitude(filename):
+    '''
+    calculate single-day largest moves
+    '''
+    index = 0
+    largestMove = 0
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        next(reader, None)  # skip header
+        rows = list(reader)
+        for row in rows[0:]:
+            try:
+                move = float(rows[index+1][4]) - float(rows[index][4])
+                if move > largestMove:
+                    largestMove = move
+                    startDate = rows[index][0]
+                    startDateClose = float(rows[index][4])
+                    endDate = rows[index+1][0]
+                    endDateClose = float(rows[index+1][4])
+                index = index + 1
+            except IndexError:
+                break
+
+        print("Largest Move: %.2f\tStart Date: %s\tStart Date Close: %.2f\tEnd Date: %s\tEnd Date Close: %.2f" % (largestMove, startDate, startDateClose, endDate, endDateClose))
+
+def main():
+    '''
+    main
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", action="store", default="vix_1999-2017.csv", metavar="csv", help="input CSV file from Yahoo Finance")
+    parser.add_argument("-d", "--days", action="store", required=False, type=int, metavar="#", help="moving window of days")
+    parser.add_argument("-m", "--multiplier", action="store", required=False, type=float, metavar="#", help="multipy by current price to get magnitude of move")
+    args = parser.parse_args()
+
+    if args.multiplier:
+        calculateRangeBasedMagnitude(args.input, args.days, args.multiplier)
+    else:
+        calculateMagnitude(args.input)
+
+'''
+send command-line arguments to main for processing
+'''
+if __name__ == "__main__":
+    main()
